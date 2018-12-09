7dd85e4d11b981c1c86a93b4272a7700e128647c
Nick Do <nkhl_linh@yahoo.com>
2018-12-10 09:00:36 +1300

    MINOR CHANGE: change responsive properties

--- src/deign system/theme.js
+++ src/deign system/theme.js
@@ -13,7 +13,7 @@ const addAliases = (arr, aliases) =>
     })
   );
 
-export const breakpoints = [400, 512, 768, 1024];
+export const breakpoints = [400, 768, 1024];
 
 export const mediaQueries = {
   ...breakpoints.map(createMediaQuery),
