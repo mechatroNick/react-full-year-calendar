fa1d39a86817571c382a8cdf7883c3760bbd2643
Nick Do <nkhl_linh@yahoo.com>
2018-12-15 19:44:34 +1300

    PERFORMANCE OPTIMIZATION: changed some components to PURE COMPONENT, helps with automatically figure out shouldComponentUpdate() by default

--- src/components/MonthCalendar.js
+++ src/components/MonthCalendar.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, Component, PureComponent } from "react";
 import {
   Box,
   Flex,
@@ -100,7 +100,7 @@ const DaysOfWeekIndicator = () => {
   );
 };
 
-class DayBoxGroup extends Component {
+class DayBoxGroup extends PureComponent {
   state = {
     showModal: false,
     date: undefined,
@@ -275,49 +275,51 @@ class DayBoxGroup extends Component {
   }
 }
 
-const MonthCalendar = props => {
-  const { month, monthSkeleton, monthEvents } = props;
-  return (
-    <Fragment>
-      <Box width={[0.1, 0.025, 1 / 54]} />
-      <Flex
-        width={[0.8, 0.45, 16 / 54]}
-        color={"Black"}
-        wrap={true}
-        textAlign={"center"}
-        align={"center"}
-      >
-        <Box width={1}>
-          <Heading.h3>{month}</Heading.h3>
-        </Box>
-        <DaysOfWeekIndicator />
-        {monthSkeleton.map(fullDate => {
-          if (fullDate === "empty") {
-            return (
-              <Box py={[1, 2, 2, 3]} my={1} color={"white"} width={1 / 7}>
-                *
-              </Box>
-            );
-          } else {
-            return (
-              <DayBoxGroup
-                key={fullDate}
-                fullDate={fullDate}
-                date={parseInt(fullDate.substring(8, 10))}
-                month={parseInt(fullDate.substring(5, 7))}
-                year={parseInt(fullDate.substring(0, 4))}
-                eventList={monthEvents[fullDate]}
-                onChangeSpecialEvent={
-                  props.handleUpdateAppStorageOnSpecialEvent
-                }
-              />
-            );
-          }
-        })}
-      </Flex>
-      <Box width={[0.1, 0.025, 1 / 54]} />
-    </Fragment>
-  );
-};
+class MonthCalendar extends PureComponent {
+  render() {
+    const { month, monthSkeleton, monthEvents, handleUpdateAppStorageOnSpecialEvent } = this.props;
+    return (
+      <Fragment>
+        <Box width={[0.1, 0.025, 1 / 54]} />
+        <Flex
+          width={[0.8, 0.45, 16 / 54]}
+          color={"Black"}
+          wrap={true}
+          textAlign={"center"}
+          align={"center"}
+        >
+          <Box width={1}>
+            <Heading.h3>{month}</Heading.h3>
+          </Box>
+          <DaysOfWeekIndicator />
+          {monthSkeleton.map(fullDate => {
+            if (fullDate === "empty") {
+              return (
+                <Box py={[1, 2, 2, 3]} my={1} color={"white"} width={1 / 7}>
+                  *
+                </Box>
+              );
+            } else {
+              return (
+                <DayBoxGroup
+                  key={fullDate}
+                  fullDate={fullDate}
+                  date={parseInt(fullDate.substring(8, 10))}
+                  month={parseInt(fullDate.substring(5, 7))}
+                  year={parseInt(fullDate.substring(0, 4))}
+                  eventList={monthEvents[fullDate]}
+                  onChangeSpecialEvent={
+                    handleUpdateAppStorageOnSpecialEvent
+                  }
+                />
+              );
+            }
+          })}
+        </Flex>
+        <Box width={[0.1, 0.025, 1 / 54]} />
+      </Fragment>
+    );
+  }
+}
 
 export default MonthCalendar;
