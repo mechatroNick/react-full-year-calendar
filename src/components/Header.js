e2e02071c32f979cfae5038da8c4737686610761
Nick Do <nkhl_linh@yahoo.com>
2018-12-16 09:01:14 +1300

    MINOR CHANGE: improve responsiveness for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,33 @@
 import React, { Fragment } from "react";
 import { Box, Button, Flex, Heading } from "../design system";
-import {TYPE_OF_CHANGE_YEAR} from  "../logic/constant"
+import { TYPE_OF_CHANGE_YEAR } from "../logic/constant";
 import PropTypes from "prop-types";
 
 const Header = props => {
-  const {year, changeYear} = props;
+  const { year, changeYear } = props;
   return (
     <Fragment>
       <Flex wrap={true} justify={"center"} align={"center"}>
-        <Box width={1 / 5} my={[3,3,4,4]} textAlign={"center"}>
-          <Button id={TYPE_OF_CHANGE_YEAR.TO_PREVIOUS_YEAR} radius={5} onClick={changeYear}>
+        <Box fontSize={[2, 3, 4, 4]} width={[1 / 3, 1/4, 1 / 5]} my={[3, 3, 4, 4]} textAlign={"center"}>
+          <Button
+
+            id={TYPE_OF_CHANGE_YEAR.TO_PREVIOUS_YEAR}
+            radius={5}
+            onClick={changeYear}
+          >
             Prev
           </Button>
         </Box>
-        <Box width={3 / 5} textAlign={"center"}>
+        <Box width={[1 / 3, 2/4, 3 / 5]} textAlign={"center"}>
           <Heading.h1>{year}</Heading.h1>
         </Box>
-        <Box width={1 / 5} my={[3,3,4,4]}  textAlign={"center"}>
-          <Button id={TYPE_OF_CHANGE_YEAR.TO_NEXT_YEAR} radius={5} onClick={changeYear}>
+        <Box fontSize={[2, 3, 4, 4]} width={[1 / 3, 1/4, 1 / 5]} my={[3, 3, 4, 4]} textAlign={"center"}>
+          <Button
+
+            id={TYPE_OF_CHANGE_YEAR.TO_NEXT_YEAR}
+            radius={5}
+            onClick={changeYear}
+          >
             Next
           </Button>
         </Box>
